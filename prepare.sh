923a32a394485a9a663141186ac063d165ac3e35
mgblow <mgblow@gmail.com>
2023-09-17 20:53:58 +0330

    updated majar initial files

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,9 @@
 virtualenv env
-source env
+# ./env/Scripts/activate : for windows
+# source env : for linux
 pip install django
 pip install django-environ
 pip install psycopg2
 pip install djangorestframework
+
+pip install zappa
